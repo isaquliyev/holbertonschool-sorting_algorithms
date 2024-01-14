f35715a6b2a0bc9c5df114c4a77d8555eb1e7107
isaquliyev <isa.quluyev.wip@gmail.com>
2024-01-14 14:44:57 -0800

    Helper function declared at header.

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+#include "print_array.c"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using the
+ * Bubble sort algorithm.
+ * This function prints all steps of Sorting process.
+ *
+ * @array: Pointer to first element of array.
+ * @size: Size of given array.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (j = 0; j < size; j++)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (i != size - 1 && array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
